fef998f91fc6e0927bde925669520229bba4302a
momo <momo76w@gmail.com>
2017-10-28 16:06:53 +0900

    create M&C

---
+++ tag-keep/app/Http/Controllers/TagController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Tag;
+
+class TagController extends Controller
+{
+    public function index()
+    {
+        return Tag::take(5)->get()->keyBy('id');
+    }
+    public function store(Request $request)
+    {
+        return Tag::create($request->only('name'))->save()->fresh();
+    }
+
+    public function destroy($id)
+    {
+        return Tag::destroy($id);
+    }
+
+    public function update($id, Request $request)
+    {
+        return Tag::find($id)->fill($request->only('is_done'))
+               ->save()->fresh();
+    }
+}
