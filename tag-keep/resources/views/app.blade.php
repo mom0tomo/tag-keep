a7f7d8cdf012e50554db138c14d8bb4b9e8b13ce
momo <momo76w@gmail.com>
2017-10-29 23:15:44 +0900

    add csrf token

--- tag-keep/resources/views/app.blade.php
+++ tag-keep/resources/views/app.blade.php
@@ -4,18 +4,19 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>TAG Keep</title>
     <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
-  </head>
-
-  <body>
-    <div id="app">
-    </div>
     <script>
       window.Laravel = <?php echo json_encode([
         'csrfToken' => csrf_token(),
       ]); ?>
     </script>
+  </head>
+
+  <body>
+    <div id="app">
+    </div>
   </body>
 
   <script src="{{asset('js/app.js')}}"></script>
