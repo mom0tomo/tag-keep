201f926797f0b95c04fc0498b06a213d48dabc28
momo <momo76w@gmail.com>
2017-10-29 13:51:47 +0900

    add test

--- tag-keep/tests/Feature/TagTest.php
+++ tag-keep/tests/Feature/TagTest.php
@@ -14,17 +14,17 @@ class TagTest extends TestCase
 
     public function testCrudTag()
     {
-        $this->json('POST', '/api/tags', ['name' => 'momo 0tomo'])
+        $this->json('POST', '/api/tags', ['name' => 'Learn Vue.js'])
              ->assertStatus(200)
              ->assertJson([
-                'id'   => 6,
-                'name' => 'momo 0tomo',
+                'id'   => 1,
+                'name' => 'Learn Vue.js',
                 'is_done' => false,
             ]);
 
         $this->assertDatabaseHas('tags', [
-                'id'   => 6,
-                'name' => 'momo 0tomo',
+                'id'   => 1,
+                'name' => 'Learn Vue.js',
                 'is_done' => false,
             ]);
 
@@ -32,31 +32,31 @@ class TagTest extends TestCase
              ->assertStatus(200)
              ->assertJson([
                 1 => [
-                    'id'   => 6,
-                    'name' => 'momo 0tomo',
+                    'id'   => 1,
+                    'name' => 'Learn Vue.js',
                     'is_done' => false,
                 ]
             ]);
 
-        $this->json('PUT', '/api/tags/6', ['is_done' => true])
+        $this->json('PUT', '/api/tags/1', ['is_done' => true])
              ->assertStatus(200)
              ->assertJson([
-                'id'   => 6,
-                'name' => 'momo 0tomo',
+                'id'   => 1,
+                'name' => 'Learn Vue.js',
                 'is_done' => true,
             ]);
 
         $this->assertDatabaseHas('tags', [
-                'id'   => 6,
-                'name' => 'momo 0tomo',
+                'id'   => 1,
+                'name' => 'Learn Vue.js',
                 'is_done' => true,
             ]);
 
-        $this->json('DELETE', '/api/tags/6')
+        $this->json('DELETE', '/api/tags/1')
              ->assertStatus(200);
 
         $this->assertDatabaseMissing('tags', [
-                'id'   => 6,
+                'id'   => 1,
             ]);
     }
 }
